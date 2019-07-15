0b8de96cefb30ce9e17034be53cb9c50caf810d6
nerorilvig <oppappiwm1919@gmail.com>
2019-07-15 22:18:19 +0900

    add dein installer in initialize script

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mkdir -p ~/.cache/dein
 cp -T -r ./config ~/.config
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/dein_installer.sh
+sh ~/dein_installer.sh ~/.cache/dein
